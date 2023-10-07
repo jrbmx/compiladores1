008c63b0cc36e5151c397c1f4633975a5909bd1d
jrbmx <146797068+jrbmx@users.noreply.github.com>
2023-10-07 15:53:20 -0600

    Update practica1.c

--- practica1.c
+++ practica1.c
@@ -15,3 +15,36 @@ typedef enum {
 } TokenType;
 
 //prueba para github
+// Función para obtener el nombre literal del tipo de token
+const char* nombreTipoToken(TokenType tipo) {
+    switch (tipo) {
+        case AND: return "AND";
+        case ELSE: return "ELSE";
+        case FALSE_TOKEN: return "FALSE";
+        case FOR: return "FOR";
+        case FUN: return "FUN";
+        case IF: return "IF";
+        case NULL_TOKEN: return "NULL";
+        case OR: return "OR";
+        case PRINT: return "PRINT";
+        case RETURN: return "RETURN";
+        case TRUE_TOKEN: return "TRUE";
+        case VAR: return "VAR";
+        case WHILE: return "WHILE";
+        case IDENTIFICADOR: return "IDENTIFICADOR";
+        case NUMERO_ENTERO: return "NUMERO_ENTERO";
+        case NUMERO_DECIMAL: return "NUMERO_DECIMAL";
+        case CADENA: return "CADENA";
+        case SIMBOLO: return "SIMBOLO";
+        case ERROR_FALTA_PUNTO_Y_COMA: return "ERROR_FALTA_PUNTO_Y_COMA";
+        case ERROR_VARIABLE_DECLARADA: return "ERROR_VARIABLE_DECLARADA";
+        case ERROR_PARENTESIS_NO_CERRADO: return "ERROR_PARENTESIS_NO_CERRADO";
+        case ERROR_LLAVE_NO_CERRADA: return "ERROR_LLAVE_NO_CERRADA";
+        case ERROR_DECLARACION_SIN_TIPO: return "ERROR_DECLARACION_SIN_TIPO";
+        case ERROR_VARIABLE_NO_DECLARADA: return "ERROR_VARIABLE_NO_DECLARADA";
+        case ERROR_DIVISION_POR_CERO: return "ERROR_DIVISION_POR_CERO";
+        case ERROR_CADENA_SIN_CERRAR: return "ERROR_CADENA_SIN_CERRAR";
+        case ERROR_CARACTER_NO_VALIDO: return "ERROR_CARACTER_NO_VALIDO";
+        default: return "DESCONOCIDO";
+    }
+}
